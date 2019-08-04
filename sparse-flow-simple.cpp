a15772c2a35a5164e508f4e80022271a7db89370
Dan Park <dpark.sports@gmail.com>
2019-08-03 20:44:46 -0700

    Update sparse-flow-simple.cpp

--- sparse-flow-simple.cpp
+++ sparse-flow-simple.cpp
@@ -6,34 +6,20 @@
 #include <opencv2/video.hpp>
 using namespace cv;
 using namespace std;
-int main(int argc, char **argv)
-{
-    const string about =
-        "This sample demonstrates Lucas-Kanade Optical Flow calculation.\n"
-        "The example file can be downloaded from:\n"
-        "  https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4";
-    const string keys =
-        "{ h help |      | print this help message }"
-        "{ @image |<none>| path to image file }";
-    CommandLineParser parser(argc, argv, keys);
-    parser.about(about);
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
-    }
-    string filename = parser.get<string>("@image");
-    if (!parser.check())
-    {
-        parser.printErrors();
-        return 0;
-    }
-    VideoCapture capture(filename);
-    if (!capture.isOpened()){
-        //error in opening the video input
-        cerr << "Unable to open file!" << endl;
-        return 0;
-    }
+
+static string current_filename;
+Mat filenameMat() {
+    string filename;
+    cin >> filename;
+    filename = "/home/pose/Pictures/s190401/" + filename;
+    current_filename = filename;
+    Mat frame = imread( filename, IMREAD_COLOR );
+    return frame;
+}
+
+int main(int argc, char **argv) {
+    bool trackFeatures = true;
+
     // Create some random colors
     vector<Scalar> colors;
     RNG rng;
@@ -44,44 +30,78 @@ int main(int argc, char **argv)
         int b = rng.uniform(0, 256);
         colors.push_back(Scalar(r,g,b));
     }
-    Mat old_frame, old_gray;
+    Mat old_gray;
     vector<Point2f> p0, p1;
     // Take first frame and find corners in it
-    capture >> old_frame;
+    Mat old_frame = filenameMat();
+
     cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
     goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
     // Create a mask image for drawing purposes
     Mat mask = Mat::zeros(old_frame.size(), old_frame.type());
+
+    // Take first frame and find corners in it
     while(true){
-        Mat frame, frame_gray;
-        capture >> frame;
+        Mat frame_gray;
+        Mat frame = filenameMat();
         if (frame.empty())
             break;
         cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-        // calculate optical flow
-        vector<uchar> status;
-        vector<float> err;
-        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
-        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15,15), 2, criteria);
-        vector<Point2f> good_new;
-        for(uint i = 0; i < p0.size(); i++)
-        {
-            // Select good points
-            if(status[i] == 1) {
-                good_new.push_back(p1[i]);
-                // draw the tracks
-                line(mask,p1[i], p0[i], colors[i], 2);
-                circle(frame, p1[i], 5, colors[i], -1);
+
+        if (p0.empty()) {
+            p0.clear();
+            p1.clear();
+            goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+        } else {
+            // calculate optical flow
+            vector<uchar> status;
+            vector<float> err;
+            TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
+            calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15,15), 2, criteria);
+
+            vector<Point2f> good_new;
+            for(uint i = 0; i < p0.size(); i++)
+            {
+                // Select good points
+                if(status[i] == 1) {
+                    good_new.push_back(p1[i]);
+                    // draw the tracks
+                    line(mask,p1[i], p0[i], colors[i], 2);
+                    circle(frame, p1[i], 5, colors[i], -1);
+                }
             }
+
+            // Now update the previous frame and previous points
+            old_gray = frame_gray.clone();
+            p0 = good_new;
         }
+
+        cv::putText(frame,
+                    current_filename,
+                    cv::Point(5,5 * 10), // Coordinates
+                    cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+                    1.0, // Scale. 2.0 = 2x bigger
+                    cv::Scalar(255,255,255), // BGR Color
+                    1 // Line Thickness (Optional)
+        );
+
         Mat img;
         add(frame, mask, img);
         imshow("Frame", img);
-        int keyboard = waitKey(30);
-        if (keyboard == 'q' || keyboard == 27)
+        char c = (char)waitKey(10);
+        if( c == 27 )
             break;
-        // Now update the previous frame and previous points
-        old_gray = frame_gray.clone();
-        p0 = good_new;
+        switch( c )
+        {
+            case 'r':
+                trackFeatures = true;
+                break;
+            case 'c':
+                p0.clear();
+                p1.clear();
+                break;
+            case 'n':
+                break;
+        }
     }
 }

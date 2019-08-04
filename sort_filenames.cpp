c5e5dbe37faca851c8d78fe952ef1b175a3f4175
Dan Park <dpark.sports@gmail.com>
2019-08-03 19:33:36 -0700

    Update sort_filenames.cpp

--- sort_filenames.cpp
+++ sort_filenames.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  transpose
 //
-//  Created by Dan Park on 6/19/19.
+//  Created by Dan Park on 8/3/19.
 //  Copyright © 2019 Dan Park. All rights reserved.
 //
 
@@ -23,45 +23,6 @@
 
 using namespace std;
 
-
-void scanString() {
-    string s1, s2;
-    cin >> s1;
-    cin >> s2;
-//    scanf("%s", &s1);
-//    scanf("%s", &s2);
-
-    int len1 = s1.size();
-    int len2 = s2.size();
-
-
-    cout << len1 << " " << len2 << endl;
-    cout << s1 + s2 << endl;
-
-    char c1 = s2[0];
-    s2[0] = s1[0];
-    s1[0] = c1;
-
-    cout << s1 << " " << s2;
-    cout << endl;
-}
-
-vector<int> parseInts(string str) {
-    stringstream ss(str);
-    string token;
-
-    vector<int> ints;
-    while (getline(ss, token, '-')) {
-        stringstream wordstream(token);
-
-        int number;
-        wordstream >> number;
-        ints.push_back(number);
-    }
-
-    return ints;
-}
-
 int parseIndex(string str) {
     stringstream ss(str);
     string token;
@@ -76,13 +37,14 @@ int parseIndex(string str) {
 }
 
 int main(int argc, const char *argv[]) {
+    int max;
+    cin >> max;
+
     vector<int> indices;
     map<int, string> filenames;
     string filename;
 
-    // 1016900
-    int max = 10;
-    for (int i = 0; i < max; ++i) {
+    for (int j = 0; j < max; ++j) {
         cin >> filename;
         int index = parseIndex(filename);
         filenames[index] = filename;
@@ -90,11 +52,8 @@ int main(int argc, const char *argv[]) {
     }
 
     sort(indices.begin(), indices.end());
-    cout << endl;
-
     for (int i = 0; i < indices.size(); ++i) {
         cout << filenames[indices[i]] << endl;
     }
-    cout << endl;
     return 0;
 }

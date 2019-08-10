cd18b5e999687f2c0bdc3b38f42c9911313f58a7
Dan Park <dpark.sports@gmail.com>
2019-08-10 10:44:53 -0700

    Update diff_timestamps.cpp

--- diff_timestamps.cpp
+++ diff_timestamps.cpp
@@ -1,105 +1,140 @@
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include <iostream>
-using namespace cv;
+//
+//  main.cpp
+//  transpose
+//
+//  Created by Dan Park on 8/3/19.
+//  Copyright © 2019 Dan Park. All rights reserved.
+//
 
-const int max_value_H = 360/2;
-const int max_value = 255;
-const String window_capture_name = "Video Capture";
-const String window_detection_name = "Object Detection";
-int low_H = 0, low_S = 0, low_V = 50;
-int high_H = max_value_H, high_S = max_value, high_V = 51;
-static void on_low_H_thresh_trackbar(int, void *)
-{
-    low_H = min(high_H-1, low_H);
-    setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-    high_H = max(high_H, low_H+1);
-    setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-    low_S = min(high_S-1, low_S);
-    setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-    high_S = max(high_S, low_S+1);
-    setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-    low_V = min(high_V-1, low_V);
-    setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-    high_V = max(high_V, low_V+1);
-    setTrackbarPos("High V", window_detection_name, high_V);
-}
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <map>
+
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#include <cassert>
+
+#include <exception>
+
+#include <algorithm>
 
 using namespace std;
-static string current_filename;
-Mat filenameMat() {
-    string filename;
-    cin >> filename;
-    filename = "/media/pose/HFS16/190630/" + filename;
-    current_filename = filename;
-    Mat frame = imread( filename, IMREAD_COLOR );
-    return frame;
+
+string parseTimestamp(string str) {
+    stringstream ss(str);
+    string index, year, month, date, hour, minute, second;
+
+
+    getline(ss, index, '-');
+    getline(ss, index, '-');
+    stringstream wordstream1(index);
+
+    getline(ss, year, '-');
+    stringstream wordstream2(year);
+
+    getline(ss, month, '-');
+    stringstream wordstream3(month);
+
+    getline(ss, date, '_');
+    stringstream wordstream4(date);
+
+    getline(ss, hour, '-');
+    stringstream wordstream5(hour);
+
+    getline(ss, minute, '-');
+    stringstream wordstream6(minute);
+
+    getline(ss, second, '.');
+    stringstream wordstream7(second);
+
+    string timestamp = year + "/" + month + "/" + date + "-" + hour + ":" + minute + ":" + second;
+    return timestamp;
 }
 
-int main(int argc, char* argv[])
-{
-    namedWindow(window_capture_name);
-    namedWindow(window_detection_name);
-    // Trackbars to set thresholds for HSV values
-    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-    createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
-    createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
-    createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
-    createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
-    Mat frame, frame_HSV, frame_threshold;
-    while (true) {
-        frame = filenameMat();
-        if(frame.empty())
-        {
-            break;
-        }
-        // Convert from BGR to HSV colorspace
-        cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
-        // Detect the object based on HSV Range Values
-        inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+tm parseTM(string str) {
+    stringstream ss(str);
+    string index, year, month, date, hour, minute, second;
+    int iyear, imonth, idate, ihour, iminute, isecond;
 
-        cv::putText(frame,
-                    current_filename,
-                    cv::Point(5,5 * 10), // Coordinates
-                    cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
-                    1.0, // Scale. 2.0 = 2x bigger
-                    cv::Scalar(255,255,255), // BGR Color
-                    1 // Line Thickness (Optional)
-        );
+    getline(ss, index, '-');
+    getline(ss, index, '-');
+    stringstream wordstream1(index);
 
-        cv::putText(frame_threshold,
-                    current_filename,
-                    cv::Point(5,5 * 10), // Coordinates
-                    cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
-                    1.0, // Scale. 2.0 = 2x bigger
-                    cv::Scalar(255,255,255), // BGR Color
-                    1 // Line Thickness (Optional)
-        );
+    getline(ss, year, '-');
+    stringstream wordstream2(year);
+    wordstream2 >> iyear;
 
-        // Show the frames
-        imshow(window_capture_name, frame);
-        imshow(window_detection_name, frame_threshold);
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27)
-        {
-            break;
-        }
+    getline(ss, month, '-');
+    stringstream wordstream3(month);
+    wordstream3 >> imonth;
+
+    getline(ss, date, '_');
+    stringstream wordstream4(date);
+    wordstream4 >> idate;
+
+    getline(ss, hour, '-');
+    stringstream wordstream5(hour);
+    wordstream5 >> ihour;
+
+    getline(ss, minute, '-');
+    stringstream wordstream6(minute);
+    wordstream6 >> iminute;
+
+    getline(ss, second, '.');
+    stringstream wordstream7(second);
+    wordstream7 >> isecond;
+
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    tm tm = *ltm;
+
+    tm.tm_year = iyear; // years count from 1900
+    tm.tm_mon = imonth - 1;    // months count from January=0
+    tm.tm_mday = idate;         // days count from 1
+    tm.tm_hour = ihour;         // days count from 1
+    tm.tm_min = iminute;         // days count from 1
+    tm.tm_sec = isecond;         // days count from 1
+    return tm;
+}
+
+ostream& operator<< (ostream& os, const tm& tm) {
+    os << tm.tm_year << "/" << tm.tm_mon << "/" << tm.tm_mday << " " << tm.tm_hour << ":" << tm.tm_min << ":" << tm.tm_sec;
+    return os;
+}
+
+int main(int argc, const char *argv[]) {
+    int max;
+    cin >> max;
+
+    vector<string> timestamps;
+    map<string, string> filenames;
+    string filename;
+
+    for (int j = 0; j < max; ++j) {
+        cin >> filename;
+        string timestamp = parseTimestamp(filename);
+
+        filenames[timestamp] = filename;
+        timestamps.push_back(timestamp);
+    }
+
+    sort(timestamps.begin(), timestamps.end());
+
+    time_t p_time = time(0);
+
+    for (int i = 0; i < timestamps.size(); ++i) {
+        string timestamp = timestamps[i];
+        string filename = filenames[timestamps[i]];
+
+        tm c_tm = parseTM(filename);
+        time_t c_time = mktime(&c_tm);
+        time_t difference = c_time - p_time;
+        p_time = c_time;
+
+        cout << timestamp  << " = " << difference << " = " << filename << endl;
     }
     return 0;
 }

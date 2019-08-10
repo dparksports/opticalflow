c161ae3e4c7eabee96b4be2a4424684b3fed8511
Dan Park <dpark.sports@gmail.com>
2019-08-10 13:44:36 -0700

    Update Gaussian-pyramid.cpp

--- Gaussian-pyramid.cpp
+++ Gaussian-pyramid.cpp
@@ -5,7 +5,7 @@
 #include "opencv2/highgui.hpp"
 
 using namespace cv;
-const char* window_name = "Pyramids Demo";
+const char* window_name = "Gaussian pyramid";
 
 using namespace std;
 static string current_filename;
@@ -18,36 +18,20 @@ Mat filenameMat() {
     return frame;
 }
 
-int main( int argc, char** argv )
-{
-    cout << "\n Zoom In-Out demo \n "
-            "------------------  \n"
-            " * [i] -> Zoom in   \n"
-            " * [o] -> Zoom out  \n"
-            " * [ESC] -> Close program \n" << endl;
-    const char* filename = argc >=2 ? argv[1] : "../data/chicky_512.png";
-    // Loads an image
+int main( int argc, char** argv ) {
     Mat src = filenameMat();
-
-    // Check if image is loaded fine
     if(src.empty()){
-        printf(" Error opening image\n");
-        printf(" Program Arguments: [image_name -- default ../data/chicky_512.png] \n");
         return -1;
     }
-    for(;;)
-    {
+
+    for(;;) {
         imshow( window_name, src );
         char c = (char)waitKey(0);
-        if( c == 27 )
-        { break; }
-        else if( c == 'i' )
-        { pyrUp( src, src, Size( src.cols*2, src.rows*2 ) );
-            printf( "** Zoom In: Image x 2 \n" );
+        if( c == 27 ) {
+            break;
         }
-        else if( c == 'o' )
-        { pyrDown( src, src, Size( src.cols/2, src.rows/2 ) );
-            printf( "** Zoom Out: Image / 2 \n" );
+        else if( c == 'd' ) {
+            pyrDown( src, src, Size( src.cols/2, src.rows/2 ) );
         }
     }
     return 0;

68dbf61ed47e0b4a7832f7d5108d52d9eb1ae022
Dan Park <dpark.sports@gmail.com>
2019-08-27 17:57:00 -0700

    Update integral-image.cpp

--- integral-image.cpp
+++ integral-image.cpp
@@ -1,57 +1,41 @@
-#include <iostream>
-#include <map>
-#include <vector>
+#include <iostream> // for standard I/O
+#include <string>   // for strings
+
+#include <opencv2/core/core.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
 using namespace std;
+using namespace cv;
 
-#define ROI_UNDEFINED -1
-
-struct ROI {
-  int top;
-  int left;
-  int width;
-  int height;
-}
-
-class LOT{
-  private:
-    vector<T> imageArray;
-    
-  public:
-    map<ROI,int> sumROILookup;
-   
-    LOT(vector<T> image) {
-      imageArray = image;
+int main(int argc, char *argv[])
+{
+    string filepath = "/home/pose/Pictures/street-pano.jpg";
+    Mat image = imread( filepath, IMREAD_COLOR );
+    if(image.empty()) {
+        return 0;
     }
- 
-    int _calculateHash(ROI region) {
-      
-    }   
-    
-    int calculateHash(ROI region) {
-      int sum = map[ROI];
-      
-      if (sum == ROI_UNDEFINED) {
-        sum = _calculateHash(region);
-        map[ROI] = sum;
-      } else {
-        return sum;
-      } 
+
+//    Mat grayImage;
+//    cvtColor(image, grayImage, COLOR_RGB2GRAY );
+//
+    Mat image32;
+    image.convertTo(image32, CV_32F);
+
+    Mat IntegralImageCV;
+    integral(image32, IntegralImageCV);
+
+    const String integral_name = "Integral";
+    const String original_name = "Original";
+    namedWindow(integral_name);
+    namedWindow(original_name);
+    imshow(original_name, image);
+    imshow(integral_name, IntegralImageCV);
+    int delay = 1000 * 60 * 2;  // 1000 * 60
+    char key = (char) waitKey(delay);
+    if (key == 'q' || key == 27)
+    {
+        return 0;
     }
-    
-    
-}
-
-int calculateROISum(vector<T> image, ROI section) {
-  
-}
-
-int main() {
-
-  for (int i = 0; i < 100; i++) {
-    
-  }  
-
-
-	cout<<"Hello";
-	return 0;
+    return 0;
 }
